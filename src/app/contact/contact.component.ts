33b1005b28eb23856beddfe68e12824355fe0946
gopi2401 <gobinath@streanflo.io>
2025-04-22 18:01:58 +0530

    feat: add emailjs integration for contact form and update UI components
    - Added @emailjs/browser dependency to package.json for email functionality.
    - Updated about.component.html to reflect a change in experience and technology focus.
    - Enhanced contact.component.html with form submission handling and validation.
    - Implemented sendEmail method in contact.component.ts to handle email sending via EmailJS.
    - Improved projects.component.html and skills.component.html for better UI presentation and responsiveness.

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
+import { environment } from '../../environments/environment';
 @Component({
   selector: 'app-contact',
   imports: [],
@@ -8,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class ContactComponent {
 
+
+  public sendEmail(e: Event) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(environment.SERVICE_ID, environment.TEMPLATE_ID, e.target as HTMLFormElement, {
+        publicKey: environment.PUBLIC_KEY,
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          if (e.target) {
+            (e.target as HTMLFormElement).reset();
+          }
+        },
+        (error) => {
+          console.log('FAILED...', (error as EmailJSResponseStatus).text);
+        },
+      );
+  }
 }
