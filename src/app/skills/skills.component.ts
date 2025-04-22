fa5529d0eb31e010eef8419d2c998470383d2c73
gopi2401 <gobinath@streanflo.io>
2025-04-22 15:20:51 +0530

    Add new skill icons and update Skills component layout for improved display

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './skills.component.css'
 })
 export class SkillsComponent {
-
+ skills = [
+  { name: 'Node.js', icon: '/stack/5968322.png' },
+  { name: 'React.js', icon: '/stack/React-icon.svg.png' },
+  { name: 'Tailwind CSS', icon: '/stack/tailwind.webp' },
+  { name: 'Express.js', icon: '/stack/express-1.png' },
+  { name: 'PostgreSQL', icon: '/stack/postgresql.png' },
+  { name: 'MongoDB', icon: '/stack/mongodb.svg' },
+  { name: 'Amazon Web Services', icon: '/stack/aws.png' },
+  { name: 'Flutter', icon: '/stack/flutter.png' },
+  ];
 }
